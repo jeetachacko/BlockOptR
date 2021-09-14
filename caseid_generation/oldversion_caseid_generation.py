1e64d92a338e810d4dcd464fda94736e0db2640d
jeetachacko <ga47sot@.tum.de>
2021-09-14 11:13:29 +0000

    New caseid gen scripts

--- caseid_generation/oldversion_caseid_generation.py
+++ caseid_generation/oldversion_caseid_generation.py
@@ -19,8 +19,6 @@ fields = ["timestamp","tx_id","Mspid", "activity_name", "function_args", "endors
 
 home_dir = "/home/ubuntu/BlockProM/log_store/"
 log_dir = ' '.join(sys.argv[1:])
-#log_dir = str(sys.argv[1])
-#usecase = str(sys.argv[2])
 full_path = home_dir + log_dir + "/csv"
 
 #csv_path is the path to the output file, in which the extracted data is written in csv format
@@ -175,184 +173,68 @@ def new_key_based_caseid(path):
 
 
 def new_activity_based_caseid(path):
-
-    #Edit this part of the code based on the use case
-    #################################################
-    #0=SCM;1=DRM;2=DV;3=EHR; 4=LAP
-    usecase=0
-
     unique_keys=[]
     file = open(path)
     reader = csv.reader((x.replace('\0', '') for x in file), delimiter=',')
-    headers = next(reader)
-    headers.append("CommonKey")
     lines = list(reader)
-    for row in lines:
-        row.append('0')
-    print(headers)
-    for i in range(len(lines)):
+    for i in range(1, len(lines)):
         lines[i][13]=0
 
     next_lines=[]
-    next_lines.append(headers.copy())
-
-        #################################################
-        #Duplicating activities
-        #Edit this part of the code based on the use case
-        #################################################
-
-        #0=SCM;1=DRM;2=DV;3=EHR
-
-
-    if usecase==1:
-        for i in range(len(lines)):
-            if lines[i][3] == 'calcRevenue':
-                readkeys=(lines[i][7].strip()).split()
-                if len(readkeys) > 0:
-                    for j in range(len(readkeys)):
-                        next_lines.append(lines[i].copy())
-                        next_lines[-1][7]=readkeys[j]
-                else:
-                    next_lines.append(lines[i].copy())
-
-            else:
+    next_lines.append(lines[0].copy())
+    for i in range(1, len(lines)):
+        rangekeys=(lines[i][9].strip()).split()
+        if len(rangekeys) > 0:
+            for j in range(len(rangekeys)):
                 next_lines.append(lines[i].copy())
+                next_lines[-1][9]=rangekeys[j]
 
-    else:
-        for i in range(len(lines)):
-            rangekeys=(lines[i][9].strip()).split()
-            if len(rangekeys) > 0:
-                for j in range(len(rangekeys)):
-                    next_lines.append(lines[i].copy())
-                    next_lines[-1][9]=rangekeys[j]
-            else:
-                next_lines.append(lines[i].copy())
-
-    #################################################
-
-    writer = csv.writer(open('%s/new_duplicatedactivities_blockchainlog.csv' % (full_path), 'w'))
+        else:
+            next_lines.append(lines[i].copy())
+    
+    writer = csv.writer(open('%s/new_rangekeyssplit_blockchainlog.csv' % (full_path), 'w'))
     writer.writerows(next_lines)
 
     lines = []
-    new_path =full_path + "/new_duplicatedactivities_blockchainlog.csv"
+    new_path =full_path + "/new_rangekeyssplit_blockchainlog.csv"
     file = open(new_path)
     reader = csv.reader((x.replace('\0', '') for x in file), delimiter=',')
     lines = list(reader)
     for i in range(1,len(lines)):
         lines[i][13]=0
+        #if (lines[i][3] == 'pushASN') or (lines[i][3] == '') or (lines[i][3] == 'ship') or (lines[i][3] == 'unload') :
+        #    funcargs=(lines[i][4].strip()).split()
+        #    lines[i][14] = funcargs[0]
 
-        #################################################
-        #Deriving the common key for all activities
-        #Edit this part of the code based on the use case
-        #################################################
-
-        #0=SCM;1=DRM;2=DV;3=EHR
-
-        #################################################
-        #0. SCM
-        if usecase==0:
-            if (lines[i][3] == 'pushASN') or (lines[i][3] == 'queryLogisticUnit') or (lines[i][3] == 'ship') or (lines[i][3] == 'unload') :
-                funcargs=(lines[i][4].strip()).split()
-                if len(funcargs) > 0:
-                    lines[i][14] = funcargs[0]
-            elif (lines[i][3] == 'queryASN'):
-                #rangekeys=(lines[i][9].strip()).split()
-                #newrangekeys=[]
-                #for x in range(len(rangekeys)):
-                #    temp=rangekeys[x]
-                #    newrangekeys.append(temp[4:])
-                rangekey=lines[i][9]
-                lines[i][14] = rangekey[4:]
-                #lines[i][14] = newrangekeys
-        #################################################
-
-        #################################################
-        #1. DRM
-        if usecase==1:
-
-            mapkeys={}
-            for k in range(1,len(lines)):
-                if (lines[k][3] == 'play'):
-                    writekeys=(lines[k][8].strip()).split()
-                    funcargs=(lines[k][4].strip()).split()
-                    if len(writekeys) > 0:
-                        mapkeys[writekeys[0]] = funcargs[0]
-
-            if (lines[i][3] == 'play') or (lines[i][3] == 'queryRightHolders') or (lines[i][3] == 'viewMetaData'):
-                funcargs=(lines[i][4].strip()).split()
-                lines[i][14] = funcargs[0]
-            elif (lines[i][3] == 'calcRevenue'):
-                readkey=lines[i][7]
-                if readkey:
-                    mapid=readkey
-                    if mapid in mapkeys:
-                        lines[i][14] = mapkeys[mapid]
-            elif (lines[i][3] == 'create'):
-                writekeys=(lines[i][8].strip()).split()
-                if len(writekeys) > 1:
-                    mapid=writekeys[1]
-                    if mapid in mapkeys:
-                        lines[i][14] = mapkeys[mapid]
-        #################################################
-
-        #################################################
-        #2. DV
-        if usecase==2:
-            readkeys=(lines[i][7].strip()).split()
-            if len(readkeys) > 0:
-                lines[i][14] = readkeys[0]
-        #################################################
-
-        #################################################
-        #3. EHR
-        if usecase==3:
-            mapkeys={}
-            for k in range(1,len(lines)):
-                if (lines[k][3] == 'grantEHRAccess'):
-                    readkeys=(lines[k][7].strip()).split()
-                    funcargs=(lines[k][4].strip()).split()
-                    if len(readkeys) > 1:
-                        mapkeys[readkeys[1]] = funcargs[0]
-
-            if (lines[i][3] == 'viewEHR'):
-                funcargs=(lines[i][4].strip()).split()
-                mapid=funcargs[0]
-                if mapid in mapkeys:
-                    lines[i][14] = mapkeys[mapid]
-            else:
-                funcargs=(lines[i][4].strip()).split()
-                lines[i][14] = funcargs[0]
-        #################################################
-        #################################################
-        #4. LAP
-        if usecase==4:
-            writekeys=(lines[i][8].strip()).split()
-            if len(writekeys) > 0:
-                lines[i][14] = writekeys[0]
-        #################################################
+    
 
     new_lines=[]
-    new_lines.append(headers.copy())
-    #new_lines.append(lines[0].copy())
+    new_lines.append(lines[0].copy())
     caseid=1
     for i in range(1, len(lines)):
         activity_name=[]
-        #removes activities that do not have a common key
-        if (lines[i][14]=='0') or (lines[i][14]==0) or (lines[i][14] is None):
-            lines[i][14]=0
-            #uncomment below line if you want caseid 0 for activities with no common key
-            #new_lines.append(lines[i].copy())
-            continue
         if lines[i][13] == 0:
             lines[i][13]=caseid
+            funcargs=(lines[i][4].strip()).split()
+            args = funcargs[0]
+            rangekey = lines[i][9]
             activity_name.append(lines[i][3])
+            print(activity_name)
+            print(caseid)
             for j in range((i+1), len(lines)):
                 if (lines[j][3] not in activity_name) and (lines[j][13] == 0):
-                    #if (lines[j][3] == 'queryASN'):
-                    #    if lines[i][14] in lines[j][14]:
-                    #        lines[j][13]=caseid
-                    #        activity_name.append(lines[j][3])
-                    if lines[i][14] == lines[j][14]:
+                    funcargsj=(lines[j][4].strip()).split()
+                    argsj = funcargsj[0]
+                    rangekeyj = lines[j][9]
+                    if lines[i][3] == 'queryASN':
+                        if (rangekey in argsj) or (rangekey in rangekeyj):
+                            lines[j][13]=caseid
+                            activity_name.append(lines[j][3])
+                            print('****')
+                            print(lines[j][3])
+                    elif (args in argsj) or (args in rangekeyj):
+                        print('****')
+                        print(lines[j][3])
                         lines[j][13]=caseid
                         activity_name.append(lines[j][3])
             caseid+=1
@@ -364,7 +246,7 @@ def new_activity_based_caseid(path):
 
 
 
-#new_key_based_caseid(csv_path)
+new_key_based_caseid(csv_path)
 new_activity_based_caseid(csv_path)
 #key_based_caseid(csv_path)
 #activity_based_caseid(csv_path)
